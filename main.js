3b57006c831e9dd939cbcf9ffad8065c187733a9
LucasChavaren <lucas.chavaren.lima@escola.pr.gov.br>
2025-09-19 07:46:20 -0300

    valorEntropia

--- main.js
+++ main.js
@@ -64,13 +64,18 @@ function geraSenha(){
     classificaSenha();
 }
 
-function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha > 11){
+    if (entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 5 && tamanhoSenha < 12){
+    } else if (entropia > 35 && tamanhoSenha < 57){
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <=5){
+    } else if (entropia <=35){
         forcaSenha.classList.add('fraca');
     }
+
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
